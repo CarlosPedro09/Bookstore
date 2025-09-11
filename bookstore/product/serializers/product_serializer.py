a030772723fc0cd734a2c10400bc487bfa7ccd4c
Carlos <carlos_pedro17@live.com>
2025-09-10 21:57:51 -0300

    adicionando viewset baseada nos serializers e fazendo testes

--- bookstore/product/serializers/product_serializer.py
+++ bookstore/product/serializers/product_serializer.py
@@ -6,6 +6,9 @@ from product.serializers.category_serializer import CategorySerializer
 
 class ProductSerializer(serializers.ModelSerializer):
     category = CategorySerializer(read_only=True, many=True)
+    categories_id = serializers.PrimaryKeyRelatedField(
+        queryset=Category.objects.all(), write_only=True, many=True
+    )
 
     class Meta:
         model = Product
@@ -16,4 +19,14 @@ class ProductSerializer(serializers.ModelSerializer):
             "price",
             "active",
             "category",
+            "categories_id",
         ]
+
+    def create(self, validated_data):
+        category_data = validated_data.pop("categories_id")
+
+        product = Product.objects.create(**validated_data)
+        for category in category_data:
+            product.category.add(category)
+
+        return product
