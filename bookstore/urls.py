8a4628a126345cc418d900f1492518ee600a7b21
Carlos <carlos_pedro17@live.com>
2025-09-22 18:58:39 -0300

    ajustando urls.py

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -1,33 +1,30 @@
-"""
-bookstore URL Configuration
+"""bookstore URL Configuration
 
-The `urlpatterns` list routes URLs to views.
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
 import debug_toolbar
+from bookstore import views
 from django.contrib import admin
 from django.urls import include, path, re_path
 from rest_framework.authtoken.views import obtain_auth_token
-from bookstore import views
 
 urlpatterns = [
-    # Debug toolbar (só funciona com DEBUG=True)
     path("__debug__/", include(debug_toolbar.urls)),
-
-    # Admin do Django
     path("admin/", admin.site.urls),
-
-    # Endpoints de orders e products (separados para não conflitar)
-    re_path(r"bookstore/(?P<version>(v1|v2))/orders/", include("order.urls")),
-    re_path(r"bookstore/(?P<version>(v1|v2))/products/", include("product.urls")),
-
-    # Autenticação via token
+    re_path("bookstore/(?P<version>(v1|v2))/", include("order.urls")),
+    re_path("bookstore/(?P<version>(v1|v2))/", include("product.urls")),
     path("api-token-auth/", obtain_auth_token, name="api_token_auth"),
-
-    # Atualização do código via POST
     path("update_server/", views.update, name="update"),
-
-    # Teste de template simples
-    path("hello/", views.hello_world, name="hello_world"),
+    path('hello/', views.hello_world, name='hello_world'),
 ]
-
